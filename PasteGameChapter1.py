15f7a806b669741c0ed330051f7372d002d06257
Gant696t04 <101918732+Gant696t04@users.noreply.github.com>
2022-09-29 19:30:56 -0400

    Game FIle
    This is the game. Not much but meh

---
+++ PasteGameChapter1.py
@@ -0,0 +1,69 @@
+import random
+
+name = input("What is your name? ")
+
+print("Hello, " + name + "!")
+
+print("Here is your Number:")
+
+print(random.randint(0,256))
+
+answer1 = input("Tell me, do you like this Program? ")
+
+print("Well of course you say " + answer1 + "!")
+
+print("Either way, let us continue.")
+
+input("So how have you been fealing recently? ")
+
+print("Hm, yes I see...")
+
+answer2 = input("Could you describe it? ")
+
+print("So, as you said, and I quote: '" + answer2 + "' so is that all?")
+input()
+
+print("So tell me " + name + " , why might you feal this way? ")
+input()
+
+print("So is that it?")
+input()
+
+print("Well I guess we can try something else. Another question.")
+input()
+
+print("What do you think of this Statement? ")
+print("Ready?")
+input()
+
+print("I'd just like to interject for a moment. What you’re referring to as Linux, is in fact, GNU/Linux, or as I’ve recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX. Many computer users run a modified version of the GNU system every day, without realizing it. Through a peculiar turn of events, the version of GNU which is widely used today is often called “Linux”, and many of its users are not aware that it is basically the GNU system, developed by the GNU Project. There really is a Linux, and these people are using it, but it is just a part of the system they use. Linux is the kernel: the program in the system that allocates the machine’s resources to the other programs that you run. The kernel is an essential part of an operating system, but useless by itself; it can only function in the context of a complete operating system. Linux is normally used in combination with the GNU operating system: the whole system is basically GNU with Linux added, or GNU/Linux. All the so-called “Linux” distributions are really distributions of GNU/Linux.")
+
+print("-------------------------------------------------------------------------------------------------------------------------")
+print("So what do you think of it?")
+input()
+
+print("Well we can do some more")
+print("What do you think " + name + ", Shall we?")
+input()
+
+print("Ah you will enjoy it")
+print("Ready?")
+input()
+
+print("Jack and Jill went up the hill with a buck and a quarter")
+input()
+print("Jill came down with $2.50")
+
+print("Funny?")
+input()
+
+print("I see. Well it appears our time is up. We shall schedule another time to meet. Just, be sure to tip next time, ok?")
+input()
+print("Ah I know you will.")
+
+print("*You exit the room, and thus... you leave the program.*")
+
+print("Program written by Wyatt Turpin. Test of Python Programming Knoledge. Written on GNU/Linux with VIM. While unneccassary, this program is Licensed under the GPL v3. Thus, no Propreitary derivitaves may be built from my Source Code.")
+print("Please send Keystroke, Enter, to Exit Program ")
+input()
+
